cc28a7e2ddbf1e54baa0c61dda9f4daa24fba6da
OncDocCoder <58113078+OncDocCoder@users.noreply.github.com>
2021-09-02 15:01:08 -0400

    Update listsoflists.py

--- listsoflists.py
+++ listsoflists.py
@@ -36,72 +36,147 @@ b = (holder, [1, 2, 3, 4, 5, 6, 7, 8, 9])
 print(b)
 
 
-ARow= [[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],\
-      [1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],\
-      [1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9]]
-BRow= [[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],\
-      [1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],\
-      [1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9]]
-CRow= [[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],\
-      [1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],\
-      [1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9]]
-DRow= [[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],\
-      [1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],\
-      [1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9]]
-ERow= [[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],\
-      [1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],\
-      [1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9]]
-FRow= [[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],\
-      [1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],\
-      [1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9]]
-GRow= [[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],\
-      [1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],\
-      [1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9]]
-HRow= [[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],\
-      [1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],\
-      [1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9]]
-IRow= [[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],\
-      [1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],\
-      [1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9]]
+A0 = [1,2,3,4,5,6,7,8,9]
+A1 = [1,2,3,4,5,6,7,8,9]
+A2 = [1,2,3,4,5,6,7,8,9]
+A3 = [1,2,3,4,5,6,7,8,9]
+A4 = [1,2,3,4,5,6,7,8,9]
+A5 = [1,2,3,4,5,6,7,8,9]
+A6 = [1,2,3,4,5,6,7,8,9]
+A7 = [1,2,3,4,5,6,7,8,9]
+A8 = [1,2,3,4,5,6,7,8,9]
 
-ARow[1] = [2]
+B0 = [1,2,3,4,5,6,7,8,9]
+B1 = [1,2,3,4,5,6,7,8,9]
+B2 = [1,2,3,4,5,6,7,8,9]
+B3 = [1,2,3,4,5,6,7,8,9]
+B4 = [1,2,3,4,5,6,7,8,9]
+B5 = [1,2,3,4,5,6,7,8,9]
+B6 = [1,2,3,4,5,6,7,8,9]
+B7 = [1,2,3,4,5,6,7,8,9]
+B8 = [1,2,3,4,5,6,7,8,9]
+
+C0 = [1,2,3,4,5,6,7,8,9]
+C1 = [1,2,3,4,5,6,7,8,9]
+C2 = [1,2,3,4,5,6,7,8,9]
+C3 = [1,2,3,4,5,6,7,8,9]
+C4 = [1,2,3,4,5,6,7,8,9]
+C5 = [1,2,3,4,5,6,7,8,9]
+C6 = [1,2,3,4,5,6,7,8,9]
+C7 = [1,2,3,4,5,6,7,8,9]
+C8 = [1,2,3,4,5,6,7,8,9]
+
+D0 = [1,2,3,4,5,6,7,8,9]
+D1 = [1,2,3,4,5,6,7,8,9]
+D2 = [1,2,3,4,5,6,7,8,9]
+D3 = [1,2,3,4,5,6,7,8,9]
+D4 = [1,2,3,4,5,6,7,8,9]
+D5 = [1,2,3,4,5,6,7,8,9]
+D6 = [1,2,3,4,5,6,7,8,9]
+D7 = [1,2,3,4,5,6,7,8,9]
+D8 = [1,2,3,4,5,6,7,8,9]
+
+E0 = [1,2,3,4,5,6,7,8,9]
+E1 = [1,2,3,4,5,6,7,8,9]
+E2 = [1,2,3,4,5,6,7,8,9]
+E3 = [1,2,3,4,5,6,7,8,9]
+E4 = [1,2,3,4,5,6,7,8,9]
+E5 = [1,2,3,4,5,6,7,8,9]
+E6 = [1,2,3,4,5,6,7,8,9]
+E7 = [1,2,3,4,5,6,7,8,9]
+E8 = [1,2,3,4,5,6,7,8,9]
+
+F0 = [1,2,3,4,5,6,7,8,9]
+F1 = [1,2,3,4,5,6,7,8,9]
+F2 = [1,2,3,4,5,6,7,8,9]
+F3 = [1,2,3,4,5,6,7,8,9]
+F4 = [1,2,3,4,5,6,7,8,9]
+F5 = [1,2,3,4,5,6,7,8,9]
+F6 = [1,2,3,4,5,6,7,8,9]
+F7 = [1,2,3,4,5,6,7,8,9]
+F8 = [1,2,3,4,5,6,7,8,9]
+
+G0 = [1,2,3,4,5,6,7,8,9]
+G1 = [1,2,3,4,5,6,7,8,9]
+G2 = [1,2,3,4,5,6,7,8,9]
+G3 = [1,2,3,4,5,6,7,8,9]
+G4 = [1,2,3,4,5,6,7,8,9]
+G5 = [1,2,3,4,5,6,7,8,9]
+G6 = [1,2,3,4,5,6,7,8,9]
+G7 = [1,2,3,4,5,6,7,8,9]
+G8 = [1,2,3,4,5,6,7,8,9]
+
+H0 = [1,2,3,4,5,6,7,8,9]
+H1 = [1,2,3,4,5,6,7,8,9]
+H2 = [1,2,3,4,5,6,7,8,9]
+H3 = [1,2,3,4,5,6,7,8,9]
+H4 = [1,2,3,4,5,6,7,8,9]
+H5 = [1,2,3,4,5,6,7,8,9]
+H6 = [1,2,3,4,5,6,7,8,9]
+H7 = [1,2,3,4,5,6,7,8,9]
+H8 = [1,2,3,4,5,6,7,8,9]
+
+I0 = [1,2,3,4,5,6,7,8,9]
+I1 = [1,2,3,4,5,6,7,8,9]
+I2 = [1,2,3,4,5,6,7,8,9]
+I3 = [1,2,3,4,5,6,7,8,9]
+I4 = [1,2,3,4,5,6,7,8,9]
+I5 = [1,2,3,4,5,6,7,8,9]
+I6 = [1,2,3,4,5,6,7,8,9]
+I7 = [1,2,3,4,5,6,7,8,9]
+I8 = [1,2,3,4,5,6,7,8,9]
+
+
+ARow = [A0, A1, A2, A3, A4, A5, A6, A7, A8]
+BRow = [B0, B1, B2, B3, B4, B5, B6, B7, B8]
+CRow = [C0, C1, C2, C3, C4, C5, C6, C7, C8]
+DRow = [D0, D1, D2, D3, D4, D5, D6, D7, D8]
+ERow = [E0, E1, E2, E3, E4, E5, E6, E7, E8]
+FRow = [F0, F1, F2, F3, F4, F5, F6, F7, F8]
+GRow = [G0, G1, G2, G3, G4, G5, G6, G7, G8]
+HRow = [H0, H1, H2, H3, H4, H5, H6, H7, H8]
+IRow = [I0, I1, I2, I3, I4, I5, I6, I7, I8]
+
+TotalByRow = [ARow, BRow, CRow, DRow, ERow, FRow, GRow, HRow, IRow]
+
+
+ARow[0] = [6]
 ARow[2] = [7]
-ARow[3]= [4]
-ARow[5]= [5]
-ARow[6]= [9]
-ARow[7]= [6]
-BRow[0] = [4]
-BRow[1] = [9]
-BRow[3] = [2]
-BRow[5] = [6]
-BRow[7] = [5]
-BRow[8] = [1]
-CRow[2] = [1]
-CRow[7] = [7]
-DRow[0] = [7]
-DRow[4] = [8]
-DRow[7] = [6]
-DRow[8] = [9]
-ERow[2] = [9]
-ERow[6] = [7]
-FRow[0] = [1]
-FRow[4] = [6]
+ARow[4] = [3]
+ARow[5] = [8]
+ARow[7] = [5]
+
+BRow[4] = [2]
+BRow[5] = [1]
+
+
+CRow[0] = [1]
+CRow[6] = [8]
+CRow[7] = [9]
+
+DRow[0] = [1]
+DRow[6] = [8]
+DRow[7] = [9]
+
+
+FRow[1] = [7]
+FRow[2] = [8]
 FRow[8] = [5]
-GRow[1] = [7]
-GRow[6] = [5]
-HRow[0] = [5]
-HRow[1] = [1]
-HRow[3] = [6]
-HRow[5] = [7]
-HRow[4] = [2]
-HRow[7] = [3]
-HRow[8] = [4]
-IRow[1] = [4]
-IRow[2] = [2]
-IRow[3] = [8]
-IRow[5] = [3]
-IRow[6] = [6]
-IRow[7] = [1]
+
+GRow[2] = [4]
+GRow[3] = [8]
+GRow[8] = [7]
+
+HRow[0] = [8]
+HRow[3] = [2]
+HRow[4] = [9]
+
+IRow[1] = [5]
+IRow[3] = [6]
+IRow[4] = [4]
+IRow[6] = [1]
+IRow[8] = [2]
+
 
 Column0 = [ARow[0], BRow[0], CRow[0], DRow[0], ERow[0], FRow[0], GRow[0], HRow[0], IRow[0]]
 Column1 = [ARow[1], BRow[1], CRow[1], DRow[1], ERow[1], FRow[1], GRow[1], HRow[1], IRow[1]]
@@ -126,91 +201,85 @@ Box7 = [DRow[6], ERow[6], FRow[6], DRow[7], ERow[7], FRow[7], DRow[8], ERow[8], 
 Box8 = [GRow[6], HRow[6], IRow[6], GRow[7], HRow[7], IRow[7], GRow[8], HRow[8], IRow[8]]
 
 TotalbyBox = [Box0, Box1, Box2, Box3, Box4, Box5, Box6, Box7, Box8]
-print(Box0)
+#print(Box0)
 
 
-
-
-# z = 0  #Get a list of the single digits and where they are in their lists.
-# holder = []
-# while z<9:
-#       a = len(ARow[z])
-#       if a==1:
-#             holder.append(ARow[z])
-#       z +=1
-# print(holder) #holder is a list of single digits
-# v = len(holder)
-# m = 0
-# while m < v: #this removes the single digits from the row
-#       print(holder[m][0])
-#       a = (holder[m][0])
-#       t = 0
-#       while t < 9:
-#             if len(ARow[t])>1:
-#                   if a in ARow[t]:
-#                         ARow[t].remove(a)
-#                         #print(t, ARow[t])
-#                   t += 1
-#             else:
-#                   #print(ARow[t])
-#                   t += 1
-#       m +=1
-# print(ARow)
-
-# ###### Trying to get columns removed of singles####
-# z = 0  #Get a list of the single digits and where they are in their lists.
-# holder = []
-# while z<9:
-#       a = len(Column0[z])
-#       if a==1:
-#             holder.append(Column0[z])
-#       z +=1
-# print(holder, 'a') #holder is a list of single digits
-# v = len(holder)
-# m = 0
-# while m < v: #this removes the single digits from the row
-#       #print(holder[m][0])
-#       a = (holder[m][0])
-#       t = 0
-#       while t < 9:
-#             if len(Column0[t])>1:
-#                   if a in Column0[t]:
-#                         Column0[t].remove(a)
-#
-#                   t += 1
-#             else:
-#                   #print(ARow[t])
-#                   t += 1
-#       m +=1
-# #print(Column0)
 
 ###### Trying to get columns removed of singles####
-z = 0  #Get a list of the single digits and where they are in their lists.
-holder = []
-while z<9:
-      a = len(Box0[z])
-      if a==1:
-            holder.append(Box0[z])
-      z +=1
-print(holder, 'a') #holder is a list of single digits
-v = len(holder)
-m = 0
-while m < v: #this removes the single digits from the row
-      #print(holder[m][0])
-      a = (holder[m][0])
-      t = 0
-      while t < 9:
-            if len(Box0[t])>1:
-                  if a in Box0[t]:
-                        Box0[t].remove(a)
+def deleting_box(boxtoclean):
+      z = 0  #Get a list of the single digits and where they are in their lists.
+      holder = []
+      while z<9:
+            a = len(boxtoclean[z])
+            if a==1:
+                  holder.append(boxtoclean[z])
+            z +=1
+      #print(holder, 'a') #holder is a list of single digits
+      v = len(holder)
+      m = 0
+      while m < v: #this removes the single digits from the row
+            #print(holder[m][0])
+            a = (holder[m][0])
+            t = 0
+            while t < 9:
+                  if len(boxtoclean[t])>1:
+                        if a in boxtoclean[t]:
+                              boxtoclean[t].remove(a)
 
-                  t += 1
-            else:
-                  #print(ARow[t])
-                  t += 1
-      m +=1
-print(Box0)
+                        t += 1
+                  else:
+                        #print(ARow[t])
+                        t += 1
+            m +=1
+      return (boxtoclean)
 
+u = 0
+while u < 10:
+      ARow = deleting_box(ARow)
+      BRow = deleting_box(BRow)
+      CRow = deleting_box(ARow)
+      DRow = deleting_box(BRow)
+      ERow = deleting_box(ERow)
+      FRow = deleting_box(FRow)
+      GRow = deleting_box(GRow)
+      HRow = deleting_box(HRow)
+      IRow = deleting_box(IRow)
+
+      r  = 0
+      while r <9:
+            #print(r, TotalByRow[r])
+            r += 1
+
+      Column0 = deleting_box(Column0)
+      Column1 = deleting_box(Column1)
+      Column2 = deleting_box(Column2)
+      Column3 = deleting_box(Column3)
+      Column4 = deleting_box(Column4)
+      Column5 = deleting_box(Column5)
+      Column6 = deleting_box(Column6)
+      Column7 = deleting_box(Column7)
+      Column8 = deleting_box(Column8)
+
+      r  = 0
+      while r <9:
+            #print(r, TotalbyColumns[r])
+            r += 1
+
+      Box0 = deleting_box(Box0)
+      Box1 = deleting_box(Box1)
+      Box2 = deleting_box(Box2)
+      Box3 = deleting_box(Box3)
+      Box4 = deleting_box(Box4)
+      Box5 = deleting_box(Box5)
+      Box6 = deleting_box(Box6)
+      Box7 = deleting_box(Box7)
+      Box8 = deleting_box(Box8)
+
+      r  = 0
+      while r <9:
+            print(r, TotalByRow[r])
+            r += 1
+      u += 1
 
 
 
